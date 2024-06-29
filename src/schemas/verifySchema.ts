4216a7ba380984ec8e7e7e1a301b5b371624176c
Mayank Bansal <mayankbansal125@gmail.com>
2024-06-29 23:15:01 +0530

    feat: complete authentication routes

--- src/schemas/verifySchema.ts
+++ src/schemas/verifySchema.ts
@@ -1,5 +1,7 @@
 import { z } from "zod";
+import { emailValidation } from "./signUpSchema";
 
 export const verifySchema = z.object({
+    email: emailValidation,
     verifyCode: z.string().length(6, { message: "Verification code must be 6 digits!" })
 })
