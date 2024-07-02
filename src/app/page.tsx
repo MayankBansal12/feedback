e3ab3a1b088a6f55e7ae289678a5f1c7dcd062ba
Mayank Bansal <mayankbansal125@gmail.com>
2024-07-02 13:34:09 +0530

    add speed insights & analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,8 +31,8 @@ export default function Home() {
               className="bg-light-secondary dark:bg-dark-secondary p-0 rounded-full size-16"
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeLinecap="round"
+                strokeLinejoin="round"
                 d="m9 12.75 3 3m0 0 3-3m-3 3v-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
               />
             </svg>
