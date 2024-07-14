eca8c76d23b1a99ecb17b646746965b5bb378747
Mayank Bansal <mayankbansal125@gmail.com>
2024-07-15 01:54:34 +0530

    fixed bugs, modified routes, schemas

--- src/app/api/v1/project/route.ts
+++ src/app/api/v1/project/route.ts
@@ -6,6 +6,7 @@ import ProjectModel from "@/models/Project";
 import { projectSchema } from "@/schemas/projectSchema";
 import { ApiResponse } from "@/types/ApiResponse";
 import { z } from "zod";
+import { ObjectId } from "mongodb";
 
 export async function GET(req: Request) {
     await dbConnect()
@@ -18,17 +19,17 @@ export async function GET(req: Request) {
     }
 
     try {
-        const clientId = req.headers.get("clientId")
-        const projects = await ProjectModel.find({ userId: clientId }).sort({ createdDate: -1 })
+        const clientId = req.headers.get("clientId") ?? ""
+        const projects = await ProjectModel.find({ userId: new ObjectId(clientId) }).sort({ createdDate: -1 })
 
-        response = { success: true, status: 200, message: "success creating a project", data: projects };
+        response = { success: true, status: 200, message: "success fetching all projects", data: projects };
         return new Response(JSON.stringify(response), { status: response.status })
     } catch (error) {
         if (error instanceof z.ZodError) {
             response = { success: false, status: 400, message: error.errors[0].message };
         } else {
-            console.error("Error creating a project!", error);
-            response = { success: false, status: 400, message: "Error creating a project!" };
+            console.error("Error fetching all projects!", error);
+            response = { success: false, status: 400, message: "Error fetching all projects!" };
         }
         return new Response(JSON.stringify(response), { status: response.status })
     }
--- src/schemas/recordSchema.ts
+++ src/schemas/recordSchema.ts
@@ -3,5 +3,5 @@ import { z } from "zod"
 export const recordSchema = z.object({
     text: z.string().optional(),
     rating: z.number().optional(),
-    formId: z.string().uuid({ message: "Invalid recordId" })
+    formId: z.string().min(10, { message: "Invalid recordId" })
 }) 
