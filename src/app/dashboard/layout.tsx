05e9ceb767f3dcd050a6064a3a183299d21afc0c
Saakshi Raut <saakshiraut28@gmail.com>
2024-07-17 13:14:00 +0530

    ui fix: the sidenav is responsive

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,19 +1,59 @@
 /** @format */
-
+"use client";
 import Nav from "@/components/nav";
 import SideNav from "@/components/sidenav";
+import { List } from "lucide-react";
+import React, { useEffect } from "react";
 
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
+  const [isSideNavOpen, setIsSideNavOpen] = React.useState(false);
+  const [isMobile, setIsMobile] = React.useState(false);
+
+  const handleResize = () => {
+    setIsMobile(window.innerWidth <= 768); // Example breakpoint for mobile
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    handleResize(); // Initial check
+
+    if (!isMobile) {
+      setIsSideNavOpen(true);
+    }
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [isMobile]);
+
   return (
     <div className="w-screen h-screen font-default">
+      <div className="top-0 absolute mx-4 my-4">
+        <button
+          onClick={() => setIsSideNavOpen(!isSideNavOpen)}
+          className={`${isMobile ? "flex" : "hidden"}`}
+        >
+          <List />
+        </button>
+      </div>
       <Nav />
-      <div className="flex h-[calc(100vh-4rem)]">
-        <div className="w-1/6">
-          <SideNav />
+      <div
+        className={`flex h-[calc(100vh-4rem)] ${isMobile ? "flex-col" : ""}`}
+      >
+        {isSideNavOpen && (
+          <div
+            className={`w-full md:w-1/6 ${
+              !isMobile || isSideNavOpen ? "" : "hidden"
+            }`}
+          >
+            <SideNav />
+          </div>
+        )}
+        <div className={`w-full ${isSideNavOpen ? "md:w-5/6" : "md:w-full"}`}>
+          {children}
         </div>
-        <div className="w-5/6">{children}</div>
       </div>
     </div>
   );
--- src/components/sidenav.tsx
+++ src/components/sidenav.tsx
@@ -2,8 +2,9 @@
 "use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import React from "react";
 
-export default function SideNav() {
+const SideNav = () => {
   const navLinks = [
     { name: "dash", link: "/dashboard" },
     { name: "projects", link: "/dashboard/projects" },
@@ -15,38 +16,43 @@ export default function SideNav() {
     { name: "settings", link: "/dashboard/settings" },
   ];
   const pathname = usePathname();
+  const [isSideNavOpen, setIsSideNavOpen] = React.useState(false);
 
   return (
-    <div className="flex flex-col justify-center bg-light-primary dark:bg-dark-primary w-full h-[calc(100vh-4rem)] text-black dark:text-white">
-      <div className="flex justify-center items-center">
-        <div className="flex flex-col justify-center items-center gap-20">
-          <div className="flex flex-col gap-2 w-full">
-            {navLinks.map((link, index) => {
-              const isActive = pathname.endsWith(link.link);
-              return (
-                <Link
-                  key={index}
-                  href={link.link}
-                  className={isActive ? `underline underline-offset-2	` : ``}
-                >
-                  {link.name}
-                </Link>
-              );
-            })}
-          </div>
-          <hr className="border-2 border-gray-400 rounded-full w-1/2" />
-          <div className="px-3 py-3 pb-8 border border-black dark:border-light-primary">
-            we care about <br />
-            your privacy so
-            <br />
-            don&apos;t worry.
-            <br />
-            everything is
-            <br />
-            safe. ^-^
+    <React.Fragment>
+      <div className="flex flex-col justify-center bg-light-primary dark:bg-dark-primary w-full h-[calc(100vh-4rem)] text-black dark:text-white">
+        <div className="flex justify-center items-center text-center md:text-left">
+          <div className="flex flex-col justify-center items-center gap-10 md:gap-20">
+            <div className="flex flex-col gap-2 w-full">
+              {navLinks.map((link, index) => {
+                const isActive = pathname.endsWith(link.link);
+                return (
+                  <Link
+                    key={index}
+                    href={link.link}
+                    className={isActive ? `underline underline-offset-2	` : ``}
+                  >
+                    {link.name}
+                  </Link>
+                );
+              })}
+            </div>
+            <hr className="border-2 border-gray-400 rounded-full w-1/2" />
+            <div className="px-3 py-3 pb-8 border border-black dark:border-light-primary">
+              we care about <br />
+              your privacy so
+              <br />
+              don&apos;t worry.
+              <br />
+              everything is
+              <br />
+              safe. ^-^
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </React.Fragment>
   );
-}
+};
+
+export default SideNav;
