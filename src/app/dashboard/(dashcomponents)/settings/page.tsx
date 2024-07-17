32ca3be87c024f5ee530297051ed5cd0e965129a
Saakshi Raut <saakshiraut28@gmail.com>
2024-07-17 21:54:29 +0530

    add: settings section

--- src/app/dashboard/(dashcomponents)/settings/page.tsx
+++ src/app/dashboard/(dashcomponents)/settings/page.tsx
@@ -3,7 +3,13 @@
 const Settings = () => {
   return (
     <div className="flex flex-col dark:bg-dark-secondary px-10 py-10 w-full h-full dark:text-white">
-      Settings (in progress, coming soon)
+      <div className="flex flex-col justify-start gap-3 my-4 w-full text-left">
+        <p className="text-xl">settings 🛠️</p>
+        <p className="text-md">
+          update your profile, change your password, and more...
+        </p>
+      </div>
+      <div className="my-4"></div>
     </div>
   );
 };
