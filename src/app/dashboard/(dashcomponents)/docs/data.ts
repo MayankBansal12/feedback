922aabc40e3f021eb54f631b22739623ee15f3db
Arghya <arghyadas242004@gmail.com>
2024-07-20 00:53:17 +0530

    add: docs section

---
+++ src/app/dashboard/(dashcomponents)/docs/data.ts
@@ -0,0 +1,25 @@
+const Data = [
+  {
+    name: "long",
+    width: "390px",
+    height: "130px",
+    link: "/forms/66958135bf993b3b2a35a95b",
+    desc: "Long forms are useful if you wish to take review out of 10 and supports for longer space",
+  },
+  {
+    name: "short",
+    width: "320px",
+    height: "90px",
+    link: "/forms/669aa03291430c9aeef92632",
+    desc: "Short forms are useful if you wish to take review out of 5 and supports for shorter space",
+  },
+  {
+    name: "big",
+    width: "320px",
+    height: "250px",
+    link: "/forms/669aa03d91430c9aeef92633",
+    desc: "Big form is useful if you wish to Modals or popups",
+  },
+];
+
+export default Data;
