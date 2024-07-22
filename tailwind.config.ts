a9522fe8381e78ba528ad6626142ee4d79f95d84
Mayank Bansal <mayankbansal125@gmail.com>
2024-07-22 22:59:40 +0530

    fix: build error & tailwind setup in pages/docs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,10 +3,10 @@ import type { Config } from "tailwindcss"
 const config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    './pages/**/*.{ts,tsx, md, mdx}',
+    './components/**/*.{ts,tsx, md, mdx}',
+    './app/**/*.{ts,tsx, md, mdx}',
+    './src/**/*.{ts,tsx, md, mdx}',
   ],
   prefix: "",
   theme: {
--- theme.config.jsx
+++ theme.config.jsx
@@ -1,6 +1,56 @@
+import { useConfig } from "nextra-theme-docs";
+
 export default {
     logo: <span>feedback docs</span>,
     project: {
         link: 'https://github.com/MayankBansal12/feedback'
-    }
+    },
+    docsRepositoryBase: "https://github.com/MayankBansal12/feedback",
+    head: () => {
+        const { frontMatter } = useConfig();
+
+        return (
+            <>
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1.0"
+                />
+                <meta
+                    property="og:title"
+                    content={frontMatter.title || "feedback"}
+                />
+                <meta
+                    property="og:description"
+                    content={
+                        frontMatter.description ||
+                        "home for collecting and managing all your user feedbacks and reviews"
+                    }
+                />
+            </>
+        );
+    },
+    footer: {
+        text: (
+            <h1>&#169;{" 2024 feedback."}</h1>
+        ),
+    },
+    editLink: {
+        text: null,
+    },
+    feedback: {
+        content: null
+    },
+    banner: {
+        key: '0.0-release',
+        text: (
+            <a href="https://feedback-easy.vercel.app" target="_blank">
+                🎉 feedback 0.0.1 is released. checkout here →
+            </a>
+        )
+    },
+    useNextSeoProps() {
+        return {
+            titleTemplate: "%s - feedback.",
+        };
+    },
 }
