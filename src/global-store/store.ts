a807e6cfbb6d544971ade53bb60f1db627b27897
Mayank Bansal <mayankbansal125@gmail.com>
2024-07-26 01:54:20 +0530

    feat: integrate project, form apis

--- src/global-store/store.ts
+++ src/global-store/store.ts
@@ -1,4 +1,4 @@
-import { UserType } from "@/types/type";
+import { FormType, ProjectType, UserType } from "@/types/type";
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
 
@@ -8,17 +8,56 @@ type UserStoreType = {
     clearUser: () => void
 }
 
-const useUserStore = create<UserStoreType>()(persist(
-    (set) => ({
-        user: null,
-        setUser: (user) => set({ user }),
-        clearUser: () => set({ user: null })
-    }),
-    {
-        name: "user-storage",
-        skipHydration: true
-    }
-))
+type ProjectStoreType = {
+    project: ProjectType | null,
+    setProject: (project: ProjectType) => void
+    clearProject: () => void
+}
+
+type FormStoreType = {
+    form: FormType | null,
+    setForm: (form: FormType) => void
+    clearForm: () => void
+}
+
+const useUserStore = create<UserStoreType>()(
+    persist(
+        (set) => ({
+            user: null,
+            setUser: (user) => set({ user }),
+            clearUser: () => set({ user: null })
+        }),
+        {
+            name: "user-storage"
+        }
+    )
+)
+
+const useProjectStore = create<ProjectStoreType>()(
+    persist(
+        (set) => ({
+            project: null,
+            setProject: (project: ProjectType) => set({ project }),
+            clearProject: () => set({ project: null }),
+        }),
+        {
+            name: 'project-storage'
+        }
+    )
+);
+
+const useFormStore = create<FormStoreType>()(
+    persist(
+        (set) => ({
+            form: null,
+            setForm: (form) => set({ form }),
+            clearForm: () => set({ form: null })
+        }),
+        {
+            name: "form-storage"
+        }
+    )
+)
 
 
-export { useUserStore }
+export { useUserStore, useProjectStore, useFormStore }
